315587046668914adcc81c1f7d0a862524f97948
Richard Dowland <rdowland@hotmail.co.uk>
2019-03-07 13:22:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments){
+  var array = [];
+  for (var i = 0; i < musicians.length; i++){
+    array.push(musicians[i] + " plays " + instruments[i]);
+  }
+  return array;
+}
+
+function johnLennonFacts(array){
+  while (array){
+    console.log(array + "!!!");
+  }
+}
