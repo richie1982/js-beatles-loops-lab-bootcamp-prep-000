61e78419ad87b02c08b958108ffb014028c88ce8
Richard Dowland <rdowland@hotmail.co.uk>
2019-03-07 13:34:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function johnLennonFacts(facts) {
 function iLoveTheBeatles(num) {
   var array = [];
   do {
-    array.push("I love the Beatles!")
+    array.push("I love the Beatles!");
     num++;
   } while (num < 15);
   return array;
