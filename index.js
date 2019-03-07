cbfbc58104f0df04d92a48a1583a088c00a2d3d8
Richard Dowland <rdowland@hotmail.co.uk>
2019-03-07 13:34:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,3 +17,11 @@ function johnLennonFacts(facts) {
   return results;
 }
 
+function iLoveTheBeatles(num) {
+  var array = [];
+  do {
+    array.push("I love the Beatles!")
+    num++;
+  } while (num < 15);
+  return array;
+}
